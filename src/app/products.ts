4b0fce79b3f0ec6677968112de254f34fc3f6d06
Jun Takeuchi <j.take14@gmail.com>
2022-08-10 14:20:29 +0900

    create front end

---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens',
+      heading1:'サンプル１',
+      heading2:'サンプル２',
+      heading3:'サンプル３',
+      headingtext1:'サンプルテキスト１サンプルテキスト１サンプルテキスト１サンプルテキスト１サンプルテキスト１',
+      headingtext2:'サンプルテキスト２',
+      headingtext3:'サンプルテキスト３'
+    },
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト１',
+      headingtext2:'サンプルテキスト２',
+      headingtext3:'サンプルテキスト３'
+    },
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      name: 'Phone Standard',
+      price: 299,
+      description: '',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト１',
+      headingtext2:'サンプルテキスト２',
+      headingtext3:'サンプルテキスト３'
+    },
+    {
+      coverImage:'./assets/img/phone-cover.jpg',
+      name: 'Phone Special',
+      price: 999,
+      description: '',
+      heading1:'サンプルテキスト１',
+      heading2:'サンプルテキスト２',
+      heading3:'サンプルテキスト３',
+      headingtext1:'サンプルテキスト１',
+      headingtext2:'サンプルテキスト２',
+      headingtext3:'サンプルテキスト３'
+    }
+  ];
+  
